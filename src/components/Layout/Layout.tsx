4feaa0438daf25b945349b04e61dfb221e721794
Pradyuman Vig <pradyumanvig@outlook.com>
2017-07-31 23:12:59 -0700

    more renaming
    Former-commit-id: 9d2ce7652082af52259c0413ff2fb9296bd5036f

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,17 +2,17 @@ import * as React from 'react'
 import * as styles from './Layout.css'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 
-interface LayoutProps {
+interface Props {
   children: [React.Component],
   params: {[key: string]: string},
   appError?: boolean
 }
 
-const Layout = ({ children, params, appError = false }: LayoutProps) => (
+const Layout = ({ children, params, appError = false }: Props) => (
   <div>
     {appError && <h1 styleName="error">Oops! Looks like something went wrong. Try refreshing the browser.</h1>}
     {React.Children.only(children)}
   </div>
 )
 
-export default withStyles<LayoutProps>(styles as any)(Layout)
+export default withStyles<Props>(styles as any)(Layout)
