04cbbe032e58075633955837a8d2f761a3dac760
Pradyuman Vig <pradyumanvig@outlook.com>
2017-08-05 21:03:08 -0700

    some work on Runes
    Former-commit-id: d86c6e4d75082379393a8e5d83692cc2a2f2d26f

--- src/components/Summoner/Runes/Runes.tsx
+++ src/components/Summoner/Runes/Runes.tsx
@@ -7,67 +7,71 @@ import * as classNames from 'classnames'
 import { Riot } from '~types'
 
 interface Props {
-  selectedMasteryPage: number,
-  selectedRunePage: number,
   onClick: (id: number, type?: string) => void,
   version: string,
   staticData: any,
   summoner: any,
 }
 
-const Runes = ({
-  selectedMasteryPage,
-  selectedRunePage,
-  onClick,
-  version,
-  staticData,
-  summoner,
-}: Props) => {
-  const runes = JSON.parse(summoner.runes)
-  const masteries = JSON.parse(summoner.masteries)
-  const runePages = runes.pages
-  const masteryPages = masteries.pages
-  const activeRunePage = runePages.find((page: Riot.RunePage) => page.id == selectedRunePage)
-  const activeMasteryPage = masteryPages.find((page: Riot.MasteryPage) => page.id == selectedMasteryPage)
+interface State {
+  selectedMasteryPage: number,
+  selectedRunePage: number,
+}
 
-  return (
-    <div className={styles.runes}>
-      <div className={styles.section}>
-        <div className={styles.part}>
-          {runePages.map((item, i) =>
-            <div
-              key={i}
-              className={classNames({
-                [styles.btn]: true,
-                [styles.selected]: selectedRunePage === item.id,
-              })}
-              onClick={() => onClick(item.id, 'runes')}
-            >
-              <span>{i}</span> | {item.name}
-            </div>,
-          )}
+class Runes extends React.Component<Props, State> {
+
+  onRunesClick = (id: number) => this.setState({ selectedRunePage: id })
+  onMasteriesClick = (id: number) => this.setState({ selectedRunePage: id })
+
+  render() {
+    const { version, summoner, staticData } = this.props
+    const { selectedRunePage, selectedMasteryPage } = this.state
+
+    const runePages: Riot.RunePage[] = JSON.parse(summoner.runes).pages
+    const masteryPages: Riot.MasteryPage[] = JSON.parse(summoner.masteries).pages
+    const activeRunePage = runePages.find((page: Riot.RunePage) => page.id == selectedRunePage)
+    const activeMasteryPage = masteryPages.find((page: Riot.MasteryPage) => page.id == selectedMasteryPage)
+
+    return (
+      <div className={styles.runes}>
+        <div className={styles.section}>
+          <div className={styles.part}>
+            {runePages.map((item, i) =>
+              <div
+                key={i}
+                className={classNames({
+                  [styles.btn]: true,
+                  [styles.selected]: selectedRunePage === item.id,
+                })}
+                onClick={() => this.onRunesClick(item.id)}
+              >
+                <span>{i}</span> | {item.name}
+              </div>,
+            )}
+          </div>
+          <div className={styles.part}>
+            {masteryPages.map((item, i) =>
+              <div
+                key={i}
+                className={classNames({
+                  [styles.btn]: true,
+                  [styles.selected]: selectedMasteryPage === item.id,
+                })}
+                onClick={() => this.onMasteriesClick(item.id)}
+              >
+                <span>{i}</span> | {item.name}
+              </div>,
+            )}
+          </div>
         </div>
-        <div className={styles.part}>
-          {masteryPages.map((item, i) =>
-            <div
-              key={i}
-              className={classNames({
-                [styles.btn]: true,
-                [styles.selected]: selectedMasteryPage === item.id,
-              })}
-              onClick={() => onClick(item.id, 'masteries')}
-            >
-              <span>{i}</span> | {item.name}
-            </div>,
-          )}
+        <div className={styles.section}>
+          <div className={styles.part}>
+          </div>
         </div>
       </div>
-      <div className={styles.section}>
-        <div className={styles.part}>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default createFragmentContainer(
